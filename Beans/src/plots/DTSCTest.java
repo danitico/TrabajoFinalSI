861bd9ba9eda50bbae3252b96aa6f542c895bf15
chema969 <chema969@hotmail.com>
2019-04-29 17:37:33 +0200

    beans

---
+++ Beans/src/plots/DTSCTest.java
@@ -0,0 +1,89 @@
+package plots;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.DynamicTimeSeriesCollection;
+import org.jfree.data.time.Second;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+/** @see http://stackoverflow.com/questions/5048852 */
+public class DTSCTest extends ApplicationFrame {
+
+    private static final String TITLE = "USO MEMORIA RAM";
+    private static final float MINMAX = 100;
+    private static final int COUNT = 2 * 60;
+    private static final int FAST = 100;
+    private static final Random random = new Random();
+    private Timer timer;
+
+    public DTSCTest(final String title) {
+        super(title);
+        final DynamicTimeSeriesCollection dataset =
+            new DynamicTimeSeriesCollection(1, COUNT, new Second());
+        dataset.setTimeBase(new Second(0, 0, 0, 1, 1, 2011));
+        dataset.addSeries(gaussianData(), 0, "Gaussian data");
+        JFreeChart chart = createChart(dataset);
+        timer.setDelay(FAST);
+      
+
+
+        this.add(new ChartPanel(chart), BorderLayout.CENTER);
+        JPanel btnPanel = new JPanel(new FlowLayout());
+        this.add(btnPanel, BorderLayout.SOUTH);
+
+        timer = new Timer(FAST, new ActionListener() {
+
+            float[] newData = new float[1];
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                newData[0] = randomValue();
+                dataset.advanceTime();
+                dataset.appendData(newData);
+            }
+        });
+    }
+
+    private float randomValue() {
+        return (float) (random.nextFloat()*100);
+    }
+
+    private float[] gaussianData() {
+        float[] a = new float[COUNT];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = randomValue();
+        }
+        return a;
+    }
+
+    private JFreeChart createChart(final XYDataset dataset) {
+        final JFreeChart result = ChartFactory.createTimeSeriesChart(
+            TITLE, "hh:mm:ss", "% de uso", dataset, true, true, false);
+        final XYPlot plot = result.getXYPlot();
+        ValueAxis domain = plot.getDomainAxis();
+        domain.setAutoRange(true);
+        ValueAxis range = plot.getRangeAxis();
+        range.setRange(0, MINMAX);
+        return result;
+    }
+
+    public void start() {
+        timer.start();
+    }
+      
+}
